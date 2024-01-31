82fafaf7c3cfebe7c63d161d51bbdc43cdef0213
Nicolas Mafra <nickmafra@hotmail.com>
2024-01-31 18:54:10 -0300

    feat: add PitchNotation

---
+++ theory/PitchNotation.mjs
@@ -0,0 +1,27 @@
+const notes = [ 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B' ];
+
+export default {
+    chromaToText(chroma) {
+        return notes[chroma];
+    },
+    textToChroma(text) {
+        const letter = text.substring(0, 1);
+        let chroma = notes.indexOf(letter);
+        if (text.includes('#')) chroma++;
+        if (text.includes('b')) chroma--;
+        return (chroma + 12) % 12;
+    },
+    pitchToText(midiNumber) {
+        const chroma = midiNumber % 12;
+        const octave = Math.floor(midiNumber / 12) - 1;
+
+        return this.chromaToText(chroma) + octave;
+    },
+    textToPitch(text) {
+        const chroma = this.textToChroma(text);
+        text = text.substring(1);
+        if (text.startsWith('#') || text.startsWith('b')) text = text.substring(1);
+        const octave = parseInt(text);
+        return 12 * (octave + 1) + chroma;
+    },
+};
